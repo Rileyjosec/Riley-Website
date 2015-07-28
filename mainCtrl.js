e11a801d7a93e9e58b77ee219ee76db3ea207806
Riley Carrasquillo <rileycarrasquillo@Rileys-MacBook-Pro.local>
2015-07-28 09:13:26 -0600

    fixed twitter timeline not loading with $timeout(function () { twttr.widgets.load(); }, 500);

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,10 +1,11 @@
 var myApp = angular.module('RiApp');
 
-app.controller ('mainCtrl', function ($scope, $rootScope, $location) {
+app.controller ('mainCtrl', function ($scope, $rootScope, $location, $timeout) {
 	$scope.home = function () {
 		$location.path('/home');
 	}
 	$scope.about = function () {
 		$location.path('/about');
 	}
+	$timeout(function () { twttr.widgets.load(); }, 500); 
 })
